bcc0e0a2175ec56f588ca8fd9ed621639a8f91b6
DESKTOP-91J1SGL\archie <archieoi@gmail.com>
2018-05-07 16:11:25 -0700

    worked on light control

---
+++ Assets/Scripts/PanelControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelControl : MonoBehaviour {
+    public Light linktoLight;
+    public bool lightControl;
+
+
+	// Use this for initialization
+	void Start () {
+        //if (lightControl == false)
+        // linktoLight.enabled = false;
+        ControlLight();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ControlLight();
+	}
+    void ControlLight() {
+        /*
+        if (lightControl == false)
+            linktoLight.enabled = false;
+            */
+            
+        linktoLight.enabled = lightControl;
+    }
+    void OnTriggerEnter(Collider other)
+        
+    {
+        Debug.Log("object enter panel trigger");
+        linktoLight.enabled = lightControl;
+        if (lightControl == true)
+        {
+
+            lightControl = false;
+        }
+        else
+        {
+            //if (myBool == true){
+            lightControl = true;
+        }
+    }
+    }
